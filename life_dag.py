6a229b7a314162a76fedc0330da209e05436b6e2
Miguel Angel <seyko.21@hotmail.com>
2020-08-04 09:37:26 -0500

    First version, load data from dataStorage

--- life_dag.py
+++ life_dag.py
@@ -21,8 +21,10 @@ from typing import Any
 
 import airflow
 from airflow import models
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.operators import python_operator
+from airflow.contrib.operators import bigquery_operator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator;
+
 
 # config variables
 #dataset for examples
@@ -34,31 +36,31 @@ dw=models.Variable.get('dw_dataset');
 #dataset mart for exploit
 dm=models.Variable.get('dm_dataset');
 
-args = {
-    'owner': 'Airflow',
+default_dag_args  = {
     'start_date': airflow.utils.dates.days_ago(2)
 }
 
-dag = models.DAG(
-    dag_id='load_data_from_files', default_args=args,
+with models.DAG(
+    dag_id='load_data_from_files_v2', default_args=default_dag_args,
     schedule_interval=None
-)
+)as dag:
 
 # [START howto_operator_gcs_to_bq]
 load_table_a = GoogleCloudStorageToBigQueryOperator(
     task_id='load_table_a',
     bucket='fs-storage-cb-sh',
-    source_objects=['/LPMXfile_table_a_YYMMDD.txt'],
+    source_objects=['LPMXfile_table_a_YYMMDD.txt'],
     field_delimiter='|',
     destination_project_dataset_table='gcp_schema_bigquery.gcp_table_a',
     write_disposition='WRITE_TRUNCATE',
     dag=dag)
 
 # [END howto_operator_gcs_to_bq]
-copy_data_to_b = BigQueryOperator(
+copy_data_to_b = bigquery_operator.BigQueryOperator(
     task_id='copy_data_to_b',
-    bucket='fs-storage-cb-sh',
-    sql='insert into {0}.TABLE_B select * from {1}.gcp_table_a '.format(dm_ex,dm_ex),
+    sql="""
+        insert into {0}.TABLE_B select * from {1}.gcp_table_a
+        """.format(dm_ex,dm_ex),
     big_query_conn_id='bigquery_default',
     write_disposition='WRITE_TRUNCATE',
     allow_large_results=True,
